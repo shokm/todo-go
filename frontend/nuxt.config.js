f3dffad9083fb155ec3af2c3af59d5e7cc0e1026
Shotaro Kimoto <26667526+shokm@users.noreply.github.com>
2022-07-12 16:27:13 +0900

    とりあえずapiからデータ取得するところまで

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -18,7 +18,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css' }
     ]
   },
 
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <Tutorial/>
+    <div>
+        <div>{{ posts }}</div>
+        <div>{{ posts.id }}</div>
+        <div>{{ posts.name }}</div>
+        <div>{{ posts.todo }}</div>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+    async asyncData({ $axios }) {
+		// 取得先のURL
+		const url = "http://localhost:4000/v1/todo/2";
+		// リクエスト（Get）
+		const response = await $axios.$get(url);
+        
+		// 配列で返ってくるのでJSONにして返却
+		return {
+			posts: response
+		};
+	}
+})
 </script>
